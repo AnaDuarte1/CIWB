6f2b289c2fe831681a4eda5911e789f82ef34fc5
Ana Beatriz Rocha Duarte <83419660+AnaDuarte1@users.noreply.github.com>
2024-09-27 22:08:40 -0300

    Add files via upload

---
+++ Semana 1/script.js
@@ -0,0 +1,6 @@
+var nome = prompt("Seu nome:");
+var idade = parseInt(prompt("Sua idade:")); // função para conveter string para número
+
+var proxima = idade + 1;
+
+alert("Olá, " + nome + "! Você tem " + idade + " anos! Ano que vem você terá " + proxima + " anos"); // Concatenação de strings
