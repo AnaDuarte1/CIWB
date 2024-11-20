0c61890297506ad4a7210018b1c6010c9041342e
Ana Beatriz Rocha Duarte <83419660+AnaDuarte1@users.noreply.github.com>
2024-11-20 10:40:11 -0300

    Update ex01.js

--- Semana 7/exercicios/ex1/ex01.js
+++ Semana 7/exercicios/ex1/ex01.js
@@ -1,12 +1,22 @@
-let botao = document.querySelector('#btn-carregar');
-botao.addEventListener('click', );
+let opcao = 'audi';
 
-document.getElementById('marca').addEventListener("change", function(event) {
-    let opcao = event.target.value; 
+function carregar() {
     let img = document.createElement('img');
     img.src = opcao + '.jpg';
     img.height = 200;
 
-    let div = document.querySelector('#main');
+    let div = document.querySelector('#mostrar');
+    div.innerHTML = '';
     div.appendChild(img);
-});
+}
+
+function mostrar() {
+        let select = document.getElementById("marca");
+        opcao = select.value; 
+        document.getElementById("selecao").innerHTML = "You selected: " + opcao;
+        carregar();  
+}
+    
+document.getElementById("marca").addEventListener('change', mostrar);
+
+carregar();
